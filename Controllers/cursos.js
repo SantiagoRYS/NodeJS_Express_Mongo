21d74715142bc3407b2ec1bd40a237b04d58eaf2
SantiagoRYS <152504184+SantiagoRYS@users.noreply.github.com>
2024-08-28 21:41:11 -0500

    feature(back-end):Agregando y probando la funcionalidad para crear cursos. Endpoint de tipo POST

--- Controllers/cursos.js
+++ Controllers/cursos.js
@@ -8,4 +8,33 @@ ruta.get('/', (req, res) => {
 });
 
 
-module.exports = ruta;
+
+
+// Función asíncrona para crear cursos
+async function crearCurso(body) {
+    let curso = new Curso({
+        titulo          : body.titulo,
+        descripcion     : body.descripcion,
+        alumnos         : body.alumnos,
+        calificacion    : body.calificacion
+    })
+    return await curso.save();
+}
+  
+// Endpoint de tipo POST para el recurso CURSOS
+ruta.post('/', (req, res) => {
+    let resultado = crearCurso(req.body);
+
+    resultado.then(curso => {
+      res.json({
+        curso
+      })
+    }).catch(err => {
+      res.status(400).json({
+        err
+      })
+    })
+  });
+
+
+  module.exports = ruta;
